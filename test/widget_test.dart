8ac6ed14c1e8dc63e04066af618ee4d2c8054e22
AGUN1505 <agun1505@gmail.com>
2024-07-30 02:21:58 +0700

    perbaikan error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:catatanku/main.dart';
+import 'package:CatatanKu/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
